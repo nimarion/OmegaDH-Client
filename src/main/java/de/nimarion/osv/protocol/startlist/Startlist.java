13d198ce085b48c522d1bd27f02269e20d6e7a09
Niklas Marion <n.marion@deutsche-glasfaser.de>
2024-06-23 09:55:28 +0200

    encoding

--- src/main/java/de/nimarion/osv/protocol/startlist/Startlist.java
+++ src/main/java/de/nimarion/osv/protocol/startlist/Startlist.java
@@ -1,13 +1,22 @@
 package de.nimarion.osv.protocol.startlist;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import com.opencsv.CSVParser;
 import com.opencsv.CSVParserBuilder;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
+import com.opencsv.exceptions.CsvValidationException;
 
 public class Startlist {
 
@@ -16,9 +25,10 @@ public class Startlist {
     public Startlist(String path) throws Exception {
         startlist = new ArrayList<>();
         String fileName = path;
+        
         CSVParser parser = new CSVParserBuilder().withSeparator(';').build();
 
-        try (CSVReader reader = new CSVReaderBuilder(new FileReader(fileName)).withCSVParser(parser).build()) {
+        try (CSVReader reader = new CSVReaderBuilder(new InputStreamReader(new FileInputStream(fileName), "Cp1252")).withCSVParser(parser).build()) {
             startlist = reader.readAll();
         }
     }
